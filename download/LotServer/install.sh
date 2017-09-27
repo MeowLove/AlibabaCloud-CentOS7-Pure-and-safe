deeb0090f67c91fc23e020326c632b5993c59875
MeowLove <32319912+MeowLove@users.noreply.github.com>
2017-09-27 17:48:27 +0800

    Update install.sh

--- download/LotServer/install.sh
+++ download/LotServer/install.sh
@@ -9,6 +9,11 @@ echo "--------------------------------------------------------------------------
 echo Press any key to continue! Exit with 'Ctrl'+'C' !
 echo -e "\n"
 sudo cd /root
+dd if=/dev/zero of=/var/swapx bs=1024 count=2097152
+mkswap /var/swapx
+chmod 0644 /var/swapx
+swapon /var/swapx
+echo '/var/swapx   swap   swap   default 0 0' >> /etc/fstab
 sudo yum install wget net-tools -y
 sudo echo 3 > /proc/sys/net/ipv4/tcp_fastopen
 sudo echo "net.ipv4.tcp_fastopen = 3" >>/etc/sysctl.conf
