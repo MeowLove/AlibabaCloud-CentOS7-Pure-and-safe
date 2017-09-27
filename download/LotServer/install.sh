69414608afcbe1cfafee4f0802cb1daecf0b1779
MeowLove <MeowLove@users.noreply.github.com>
2017-09-27 17:20:02 +0800

    fix
    fix

--- download/LotServer/install.sh
+++ download/LotServer/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "Welcome to LotServer_install tool ! V1.1"
+echo "Welcome to LotServer_install tool ! V1.2"
 echo "System requirements: CentOS 7+"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from linuxsoft.cxthhhhh.com - 2017/09/10 - MeowLove"
+echo "from blog.cxthhhhh.com - 2017/09/27 - MeowLove"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo Press any key to continue! Exit with 'Ctrl'+'C' !
 echo -e "\n"
@@ -12,19 +12,19 @@ sudo cd /root
 sudo yum install wget net-tools -y
 sudo echo 3 > /proc/sys/net/ipv4/tcp_fastopen
 sudo echo "net.ipv4.tcp_fastopen = 3" >>/etc/sysctl.conf
-rpm -ivh https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm --force
+rpm -ivh https://raw.githubusercontent.com/MeowLove/AlibabaCloud-CentOS7-Pure-and-safe/master/download/LotServer/kernel/kernel-3.10.0-327.el7.x86_64.rpm --force
 rpm -qa | grep kernel
 echo -e "\n\n\n"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "End to LotServer_install tool ! V1.1"
+echo "End to LotServer_install tool ! V1.2"
 echo "Need to restart before proceeding to the next step."
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "The kernel has been replaced by: kernel-3.10.0-327.el7.x86_64."
 echo "TCP FAST OPEN is on. But the installation of LotServer need to restart."
 echo "If you need to continue, copy the following command and proceed after a reboot."
-echo "sudo curl -sSL https://linuxsoft.cxthhhhh.com/download/CentOS/7/LotServer/install_step2.sh | sudo bash"
+echo "sudo curl -sSL https://raw.githubusercontent.com/MeowLove/AlibabaCloud-CentOS7-Pure-and-safe/master/download/LotServer/install_step2.sh | sudo bash"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from linuxsoft.cxthhhhh.com - 2017/09/10 - MeowLove"
+echo "from blog.cxthhhhh.com - 2017/09/27 - MeowLove"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo Please yum update and reboot !
+echo Please reboot !
 echo -e "\n"
--- download/kill/Snapshot_image.sh
+++ download/kill/Snapshot_image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "Welcome to Ali cloud purification tool ! V1.2"
+echo "Welcome to Ali cloud purification tool ! V1.3"
 echo "System requirements: CentOS 7+"
 echo "[*]The restored mirror must be made by AliyunDun_CentOS7_New_installation."
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from linuxsoft.cxthhhhh.com - 2017/09/10 - MeowLove"
+echo "from blog.cxthhhhh.com - 2017/09/27 - MeowLove"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo Press any key to continue! Exit with 'Ctrl'+'C' !
 echo -e "\n"
 sudo cd /root
 sudo bash /usr/local/aegis/uninstall.sh
-sudo curl -sSL https://linuxsoft.cxthhhhh.com/download/CentOS/7/AliYunDun/quartz_uninstall.sh | sudo bash
+sudo curl -sSL https://raw.githubusercontent.com/MeowLove/AlibabaCloud-CentOS7-Pure-and-safe/master/download/kill/quartz_uninstall.sh | sudo bash
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "uninstall aegis quartz aliservice  is [OK]"
 echo "---------------------------------------------------------------------------------------------------------------------"
@@ -23,17 +23,16 @@ echo "--------------------------------------------------------------------------
 echo "delete aegis quartz aliservice file  is [OK]"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo -e "\n"
-sudo /appex/bin/serverSpeeder.sh renewLic
-sudo /appex/bin/serverSpeeder.sh restart
+sudo curl -sSL https://raw.githubusercontent.com/MeowLove/AlibabaCloud-CentOS7-Pure-and-safe/master/download/LotServer/install_step2.sh | sudo bash
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "LotServer is Reset the license  is [OK]"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo -e "\n\n\n"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "End to Ali cloud purification tool ! V1.2"
+echo "End to Ali cloud purification tool ! V1.3"
 echo "Has now completed the Ali Yun International CentOS 7 pure purification work."
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from linuxsoft.cxthhhhh.com - 2017/09/10 - MeowLove"
+echo "from blog.cxthhhhh.com - 2017/09/27 - MeowLove"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo Please reboot !
 echo -e "\n"
